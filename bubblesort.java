3a3e2eea3ffaf8f19e44c0b0e36a25aa859105fd
wishyourcode <vishalm3101@gmail.com>
2025-06-14 23:43:36 +0530

    bublesort

---
+++ bubblesort.java
@@ -0,0 +1,25 @@
+public class bubblesort {
+
+    public static void main(String[] args) {
+        int[] arr = { -5, -14, 0, 5, 2, 9, 1, -95, 1, 6 };
+        bubble_Sort(arr);
+        System.out.println("Sorted array:" + java.util.Arrays.toString(arr));
+    }
+
+    static void bubble_Sort(int[] arr) {
+        boolean isSorted = false;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    isSorted = true;
+                }
+                if (!isSorted) {
+                    break; // If no swaps were made, the array is sorted
+                }
+            }
+        }
+    }
+}
