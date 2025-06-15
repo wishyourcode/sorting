52ba5c2dde6a5b86c149a26364bda6d864037122
wishyourcode <vishalm3101@gmail.com>
2025-06-16 01:25:12 +0530

    merge it

---
+++ insersionsort.java
@@ -0,0 +1,22 @@
+public class insersionsort {
+    public static void main(String[] args) {
+        int[] arr = { -5, -14, 0, 5, 2, 9, 1, -95, 1, 6 };
+        insertion_Sort(arr);
+        System.out.println("Sorted array: " + java.util.Arrays.toString(arr));
+    }
+
+    static void insertion_Sort(int[] arr) {
+       for (int i =0; i<arr.length; i++){
+        for (int j =i+1;j >0; j++ ){
+            if (arr[j]< arr[j-1]){
+                // Swap arr[j] and arr[j-1]
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+            } else {
+                break; // If the current element is in the correct position, break the inner loop
+            }
+        }
+       }
+    }
+}
